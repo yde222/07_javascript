f9e43a6641479573072ad26f505ef8739a0926fa
nyaeon <nyaooe@gmail.com>
2025-04-29 17:26:56 +0900

    25-04-29 실습

---
+++ 16_LIMNAYEON/01_javascript-core-lecture-source/01_variavle/04_explicit-coercion/03_convert-to-boolean.js
@@ -0,0 +1,23 @@
+// 논리 타입으로 명시적 변환
+// 1. Boolean 생성자 함수를 new 연산자 없이 호출
+console.log(Boolean('JavaScript')); // true
+console.log(Boolean('')); // false
+console.log(Boolean(1)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(Infinity)); // true
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)) // flase
+console.log(Boolean({})); // true
+console.log(Boolean([])); // true
+
+// 2. !부정 논리 연산자를 두번 사용하는 방법
+console.log(!!`JavaScript`);
+console.log(!!'');
+console.log(!!1);
+console.log(!!0);
+console.log(!!NaN);
+console.log(!!Infinity);
+console.log(!!undefined);
+console.log(!!{});
+console.log(!!{});
