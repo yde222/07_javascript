87187fbf535c71109aa3dd796579a8e6ed2ea808
heechae <chaehee15@naver.com>
2025-04-29 14:51:20 +0900

    0429 수업파일2

---
+++ 14_LEECHAEHEE/01_javascript-core-lecture-source/02_operator/01_comparison-operator/01_comparison-operator.js
@@ -0,0 +1,21 @@
+// 비교 연산자
+// 동등비교 (==, !=) : 암묵적 타입변환을 통해 타입 일치 시킨 후 같은 값인지 비교
+// 일치비교 (===, !==) : 타입과 값이 모두 일치하는지 비교
+
+// 숫자1, 문자 '1', 논리값 true 비교
+console.log(`1 == '1' : ${1 == '1'}`);
+console.log(`1 == true : ${1 == true}`);
+console.log(`1 === '1' : ${1 === '1'}`);
+console.log(`1 === true : ${1 === true}`);
+
+// 숫자 0, 문자 '0', 논리값 false, 빈문자열 '' 비교
+console.log(`0 == '0' : ${0 == '0'}`);
+console.log(`0 == false : ${0 == false}`);
+console.log(`0 == '' : ${0 == ''}`);
+console.log(`0 === '0' : ${0 === '0'}`);
+console.log(`0 === false : ${0 === false}`);
+console.log(`0 === '' : ${0 === ''}`);
+
+// null, undefined 비교
+console.log(`null == undefined : ${null == undefined}`);
+console.log(`null === undefined : ${null === undefined}`);
