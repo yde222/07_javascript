f4084da5a9215fa39d4b164bda734d314a247e5c
seungje_yu <coffit23@gmail.com>
2025-04-29 12:50:46 +0900

    5th period upload

--- 00_YUSEUNGJE/01_javascript-core-lecture-source/01_variable/01_data-type/05_symbol-and-object.js
+++ 00_YUSEUNGJE/01_javascript-core-lecture-source/01_variable/01_data-type/05_symbol-and-object.js
@@ -4,4 +4,9 @@ var key = Symbol('key');
 console.log(`key`, typeof key);
 
 // 객체 타입
-// 
+// 자바스크립트의 데이터 타입은 크게 원시타입, 객체 타입으로 분류
+// 객체(함수, 배열,...)에 대해서는 추후에 학습
+var obj = {};
+obj[key] = 'value'; // <--- [key]는 위에있는 심볼객체
+console.log(`obj :`, obj);
+console.log(`obj[key] >>>`, obj[key]);
