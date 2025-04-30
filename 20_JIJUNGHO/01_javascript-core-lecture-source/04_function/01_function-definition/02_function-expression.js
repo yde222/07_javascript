9b4aa20cdde1df29c8a679b306075f837230369e
jungho <kiik52@naver.com>
2025-04-30 09:34:19 +0900

    javaScript - 함수 표현식

---
+++ 20_JIJUNGHO/01_javascript-core-lecture-source/04_function/01_function-definition/02_function-expression.js
@@ -0,0 +1,17 @@
+// 함수 표현식
+// 자바스크립트의 함수는 객체 타입의 값의 성질을 갖는 "일급 객체"이다.
+// 함수 리터럴로 생성한 객체를 변수에 할당할 수 있다.
+
+var hello = function (name) {
+  return `${name}님 안녕~`;
+};
+console.log(hello("호야"));
+
+// 함수 표현식에서 함수명을 생략하지 않아도 문제는 없다.
+var calc = function add(a, b) {
+  return a + b;
+};
+
+// 단, 함수 호출은 식별자로 이루어지며, 함수명으로는 호출 할 수 없다.
+console.log(calc(1, 2));
+// console.log(add(1, 2)); -> ReferenceError: a is not defined
